c4e34ee113af04aa6f16e646cfe63623dd793263
unta1337 <firstapple42@gmail.com>
2021-08-30 18:26:35 +0000

    Add p4900

---
+++ p4900/p4900.c
@@ -0,0 +1,26 @@
+// 4900: 7 더하기
+/*
+ * 두 개의 7 세그먼트 입력이 주어질 때, 수 두의 합의 7 세그먼트 형식을 구하여라.
+ */
+// https://www.acmicpc.net/problem/4900
+
+#include <stdio.h>
+
+const char* seven_seg[] =
+{
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	""
+};
+
+int main(void)
+{
+	return 0;
+}
