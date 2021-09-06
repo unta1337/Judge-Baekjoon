2143706af59a4bbc641da9379e45936c393eccfc
unta1337 <firstapple42@gmail.com>
2021-09-06 09:36:33 +0000

    p2557 결과 추가

--- p2557/p2557.c
+++ p2557/p2557.c
@@ -4,6 +4,11 @@
  */
 // https://www.acmicpc.net/problem/2557
 
+// 메모리: 1112 KB
+// 시간: 0 ms
+// 코드 길이: 182 bytes
+// http://boj.kr/eddcc32b73dc462fa1c36ee189598f39
+
 #include <stdio.h>
 
 int main(void)
