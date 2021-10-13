568b6441ad26b29ebdadf49ae916a18293212b4f
unta1337 <firstapple42@gmail.com>
2021-10-13 18:04:23 +0900

    p1436 최초 완료

--- p1436/p1436.c
+++ p1436/p1436.c
@@ -4,9 +4,65 @@
  */
 // https://www.acmicpc.net/problem/1436
 
+void get_digits(int number, int digits[])
+{
+	int index = 0;
+	while (number > 0)
+	{
+		digits[index++] = number % 10;
+		number /= 10;
+	}
+}
+
+int six_in_a_row(int digits[], int size)
+{
+	int max_count = 0;
+
+	int six_count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (digits[i] == 6)
+			six_count++;
+		else
+		{
+			max_count = six_count > max_count ? six_count : max_count;
+			six_count = 0;
+		}
+	}
+
+	return max_count;
+}
+
+int get_movie_name(int n)
+{
+	int count = 0;
+	int six_count = 0;
+	int name = 0;
+
+	for (int i = 666; count < n; i++)
+	{
+		int digits[20] = { };
+		get_digits(i, digits);
+
+		six_count = six_in_a_row(digits, 20);
+		if (six_count >= 3)
+		{
+			name = i;
+			count++;
+		}
+	}
+
+	return name;
+}
+
 #include <stdio.h>
 
 int main(void)
 {
+	int n;
+	scanf("%d", &n);
+
+	printf("%d\n", get_movie_name(n));
+
 	return 0;
 }
