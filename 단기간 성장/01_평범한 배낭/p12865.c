338e06742a0e179071d820c556dfa556aa897a3c
S.Y. Kim <firstapple42@gmail.com>
2021-11-30 08:37:41 +0900

    단기간 성장 시리즈 추가, 12865번 문제 추가

---
+++ 단기간 성장/01_평범한 배낭/p12865.c
@@ -0,0 +1,12 @@
+// 12865: 평범한 배낭
+/*
+ * 물품의 무게와 가치, 배낭의 최대 적재 용량이 주어졌을 때, 가능한 가치의 최댓값을 구하여라.
+ */
+// https://www.acmicpc.net/problem/12865
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
