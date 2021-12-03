2fa32abb03af6fbdbd1063b380cf0b05eae7da17
unta1337 <firstapple42@gmail.com>
2021-12-04 00:46:46 +0900

    p3197: 기본적인 틀 구현

---
+++ 단기간 성장/03_p3197/p3197.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_grid(char** grid, int rows, int cols)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 17; j++)
+            printf("%c", grid[i][j]);
+        printf("\n");
+    }
+}
+
+int* find_swan(char** grid, int rows, int cols)
+{
+    static int result[2];
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            if (grid[row][col] == 'L')
+            {
+                result[0] = row;
+                result[1] = col;
+
+                return (int*)&result;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+int am_i_melt(char** grid, int rows, int cols, int row, int col)
+{
+    if (grid[row][col] == '.')
+        return 1;
+    if (grid[row][col] == 'L')
+        return 0;
+
+    for (int row_diff = -1; row_diff <= 1; row_diff++)
+    {
+        int row_index = row + row_diff;
+
+        if (row_index < 0 || row_index >= rows)
+            continue;
+
+        if (grid[row_index][col] == '.')
+            return 1;
+    }
+
+    for (int col_diff = -1; col_diff <= 1; col_diff++)
+    {
+        int col_index = col + col_diff;
+
+        if (col_index < 0 || col_index >= cols)
+            continue;
+
+        if (grid[row][col_index] == '.')
+            return 1;
+    }
+
+    return 0;
+}
+
+void next_iteration(char** grid, int rows, int cols)
+{
+    char** next_grid = (char**)malloc(rows * sizeof(char*));
+    for (int i = 0; i < rows; i++)
+    {
+        next_grid[i] = (char*)malloc(cols * sizeof(char));
+        memcpy(next_grid[i], grid[i], cols * sizeof(char));
+    }
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            if (am_i_melt(grid, rows, cols, row, col))
+                next_grid[row][col] = '.';
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        memcpy(grid[i], next_grid[i], cols * sizeof(char));
+        free(next_grid[i]);
+    }
+    free(next_grid);
+}
+
+int main()
+{
+    char test_grid[8][17] =
+    {
+        { '.', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.', 'X', 'X', '.', 'X', 'X', 'X' },
+        { '.', '.', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', '.', 'X', 'X', 'X' },
+        { '.', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.' },
+        { '.', '.', 'X', 'X', 'X', 'X', 'X', '.', 'L', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.' },
+        { '.', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.' },
+        { 'X', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.', '.', 'X', 'X', 'X', 'X', '.', '.', '.' },
+        { '.', '.', 'X', 'X', 'X', 'X', 'X', '.', '.', '.', 'X', 'X', 'X', '.', '.', '.', '.' },
+        { '.', '.', '.', '.', 'X', 'X', 'X', 'X', 'X', '.', 'X', 'X', 'X', 'L', '.', '.', '.' }
+    };
+
+    int rows = 8;
+    int cols = 17;
+
+    char** grid = (char**)malloc(rows * sizeof(char*));
+    for (int i = 0; i < rows; i++)
+        grid[i] = test_grid[i];
+
+    int* swan = find_swan(grid, 8, 17);
+    printf("Swan: %d, %d\n\n", swan[0], swan[1]);
+
+    print_grid(grid, 8, 17);
+    printf("\n");
+
+    next_iteration(grid, 8, 17);
+
+    print_grid(grid, 8, 17);
+    printf("\n");
+
+    next_iteration(grid, 8, 17);
+
+    print_grid(grid, 8, 17);
+    printf("\n");
+
+    free(grid);
+
+    return 0;
+}
