7495506784fdc845bbc04d8d707417efdb59b41e
unta1337 <firstapple42@gmail.com>
2022-01-24 17:31:53 +0900

    p10818: 최소, 최대 추가

---
+++ p10818/p10818.c
@@ -0,0 +1,35 @@
+// 10818: 최소, 최대
+/*
+ * n개의 정수가 주어질 때, 최솟값과 최댓값을 구하여라.
+ */
+// https://www.acmicpc.net/problem/9613
+
+// 메모리: 1112 KB
+// 시간: 184 ms
+// 코드 길이: 510 bytes
+// http://boj.kr/a88011746d764671b02bdeaf139c3b2c
+
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+    int min = INT_MAX;
+    int max = -INT_MAX;
+
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        int input;
+        scanf("%d", &input);
+
+        min = input < min ? input : min;
+        max = input > max ? input : max;
+    }
+
+    printf("%d %d", min, max);
+
+    return 0;
+}
