e069e0ce8c40fa03070ab11b3cc94ac1c3161f97
unta1337 <firstapple42@gmail.com>
2022-01-26 21:12:34 +0900

    p10926: ??! 추가

---
+++ p10926/p10926.c
@@ -0,0 +1,22 @@
+// 10926: ??!
+/*
+ * 이미 존재하는 아이디가 주어졌을 때, 이에 대해 놀라움을 표현하여라.
+ */
+// https://www.acmicpc.net/problem/10926
+
+// 메모리: 1112 KB
+// 시간: 0 ms
+// 코드 길이: 289 bytes
+// http://boj.kr/1a50d4bb48f246f7bede5a0c3fae4976
+
+#include <stdio.h>
+
+int main(void)
+{
+    char input[51];
+    scanf("%s", input);
+
+    printf("%s??!\n", input);
+
+    return 0;
+}
