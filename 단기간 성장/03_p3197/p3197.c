43fd256af5357ae5af6fd7f5cf0990e64a259904
unta1337 <firstapple42@gmail.com>
2022-01-29 01:24:04 +0900

    p3197: 호수 갱신용 함수 추가

--- 단기간 성장/03_p3197/p3197.c
+++ 단기간 성장/03_p3197/p3197.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 백조의 호수의 그리드 구조체.
 typedef struct grid
 {
     int rows;
@@ -12,6 +13,7 @@ grid_t* create_grid(int rows, int cols);
 void delete_grid(grid_t* this);
 void print_grid(grid_t* grid);
 
+// BFS 로직을 위한 큐 구조체.
 typedef struct queue
 {
     int size;
@@ -25,6 +27,10 @@ void q_push(queue_t* this, int pair[2]);
 void q_pop(queue_t* this);
 void q_print(queue_t* this);
 
+// 호수 갱신용 함수.
+char get_next_cell(grid_t* grid, int location[2]);      // 특정 셀의 다음 상태를 반환.
+void next_grid(grid_t* grid);                           // 모든 셀을 다음 상태로 갱신.
+
 int main(void)
 {
     char test_grid[8][17] =
@@ -32,11 +38,11 @@ int main(void)
         { '.', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.', 'X', 'X', '.', 'X', 'X', 'X' },
         { '.', '.', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', '.', 'X', 'X', 'X' },
         { '.', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.' },
-        { '.', '.', 'X', 'X', 'X', 'X', 'X', '.', 'L', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.' },
+        { '.', '.', 'X', 'X', 'X', 'X', 'X', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.' },
         { '.', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.' },
         { 'X', 'X', 'X', 'X', 'X', 'X', 'X', '.', '.', '.', 'X', 'X', 'X', 'X', '.', '.', '.' },
         { '.', '.', 'X', 'X', 'X', 'X', 'X', '.', '.', '.', 'X', 'X', 'X', '.', '.', '.', '.' },
-        { '.', '.', '.', '.', 'X', 'X', 'X', 'X', 'X', '.', 'X', 'X', 'X', 'L', '.', '.', '.' }
+        { '.', '.', '.', '.', 'X', 'X', 'X', 'X', 'X', '.', 'X', 'X', 'X', '.', '.', '.', '.' }
     };
 
     grid_t* grid = create_grid(8, 17);
@@ -47,6 +53,16 @@ int main(void)
             grid->grid[row][col] = test_grid[row][col];
         }
     }
+
+    print_grid(grid);
+
+    next_grid(grid);
+    printf("\n");
+
+    print_grid(grid);
+
+    next_grid(grid);
+    printf("\n");
 
     print_grid(grid);
 
@@ -138,4 +154,49 @@ void q_print(queue_t* this)
 {
     for (int i = 0; i < this->size; i++)
         printf("(%d, %d) \n", this->pairs[i][0], this->pairs[i][1]);
+}
+
+char get_next_cell(grid_t* grid, int location[2])
+{
+    int row = location[0], col = location[1];
+
+    char cell = grid->grid[row][col];
+    if (cell != 'X')
+        return cell;
+
+    for (int row_diff = -1; row_diff <= 1; row_diff++)
+    {
+        int row_index = row + row_diff;
+        if (!(0 <= row_index && row_index < grid->rows))
+            continue;
+
+        if (grid->grid[row_index][col] == '.')
+            return '.';
+    }
+
+    for (int col_diff = -1; col_diff <= 1; col_diff++)
+    {
+        int col_index = col + col_diff;
+        if (!(0 <= col_index && col_index < grid->cols))
+            continue;
+
+        if (grid->grid[row][col_index] == '.')
+            return '.';
+    }
+
+    return 'X';
+}
+
+void next_grid(grid_t* grid)
+{
+    grid_t* next = create_grid(grid->rows, grid->cols);
+    for (int row = 0; row < grid->rows; row++)
+        for (int col = 0; col < grid->cols; col++)
+            next->grid[row][col] = get_next_cell(grid, (int[2]){ row, col });
+
+    for (int row = 0; row < grid->rows; row++)
+        for (int col = 0; col < grid->cols; col++)
+            grid->grid[row][col] = next->grid[row][col];
+    
+    delete_grid(next);
 }
